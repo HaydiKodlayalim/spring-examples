11c96fa42b3529cc146154873b601fc1b6666be7
tanertemel <taner.temel@coyoapp.com>
2019-12-14 20:41:22 +0100

    spring boot rabbitmq örnek

--- spring-boot-rabbitmq/src/main/java/com/haydikodlayalim/sr/RabbitApplication.java
+++ spring-boot-rabbitmq/src/main/java/com/haydikodlayalim/sr/RabbitApplication.java
@@ -1,6 +1,5 @@
 package com.haydikodlayalim.sr;
 
-import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 

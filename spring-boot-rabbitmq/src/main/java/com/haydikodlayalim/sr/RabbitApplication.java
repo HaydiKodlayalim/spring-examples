674c95c06b8ff6bc110dbabb5a8d4adae73a8828
tanertemel <taner.temel@coyoapp.com>
2019-12-14 20:41:05 +0100

    spring boot rabbitmq örnek

---
+++ spring-boot-rabbitmq/src/main/java/com/haydikodlayalim/sr/RabbitApplication.java
@@ -0,0 +1,13 @@
+package com.haydikodlayalim.sr;
+
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitApplication.class, args);
+    }
+}
---
+++ spring-boot-rabbitmq/src/main/java/com/haydikodlayalim/sr/producer/NotificationProducer.java
@@ -0,0 +1,40 @@
+package com.haydikodlayalim.sr.producer;
+
+import com.haydikodlayalim.sr.model.Notification;
+import java.util.Date;
+import java.util.UUID;
+import javax.annotation.PostConstruct;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+public class NotificationProducer {
+
+    @Value("${sr.rabbit.routing.name}")
+    private String routingName;
+
+    @Value("${sr.rabbit.exchange.name}")
+    private String exchangeName;
+
+
+    @PostConstruct
+    public void init() {
+        Notification notification = new Notification();
+        notification.setNotificationId(UUID.randomUUID().toString());
+        notification.setCreatedAt(new Date());
+        notification.setMessage("Haydi Kodlayalim platformuna hosgeldiniz");
+        notification.setSeen(Boolean.FALSE);
+
+        sendToQueue(notification);
+    }
+
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+    public void sendToQueue(Notification notification) {
+        System.out.println("Notification Sent ID : " + notification.getNotificationId());
+        rabbitTemplate.convertAndSend(exchangeName, routingName, notification);
+    }
+}

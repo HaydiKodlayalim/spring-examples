6eab2fb8c65ec09b51fad8607c463c45682d3c50
sofisticat <serhatuyanmis@gmail.com>
2021-04-18 16:56:16 +0300

    Her 5 saniyede mesaj yollama fonksiyonu eklendi

--- spring-boot-rabbitmq/src/main/java/com/haydikodlayalim/sr/handler/NotificationSender.java
+++ spring-boot-rabbitmq/src/main/java/com/haydikodlayalim/sr/handler/NotificationSender.java
@@ -1,7 +1,7 @@
-package demo;
+package com.haydikodlayalim.sr.handler;
 
-import demo.model.Notification;
-import demo.producer.Producer;
+import com.haydikodlayalim.sr.model.Notification;
+import com.haydikodlayalim.sr.producer.NotificationProducer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -13,16 +13,16 @@ import java.util.UUID;
 public class NotificationSender {
 
     @Autowired
-    private Producer producer;
+    private NotificationProducer producer;
 
     public void getThread() {
         Thread thread = new Thread(() -> {
             while (true) {
                 Notification notification = new Notification();
-                notification.setId(UUID.randomUUID().toString());
+                notification.setNotificationId(UUID.randomUUID().toString());
                 notification.setCreatedAt(new Date());
-                notification.setMessage("New message from sofisticat...");
-                notification.setSent(false);
+                notification.setMessage("Haydi kodlayalımdan mesaj var...");
+                notification.setSeen(false);
                 producer.sendToQueue(notification);
                 try {
                     Thread.sleep(5000);
